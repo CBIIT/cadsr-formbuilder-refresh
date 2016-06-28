a27ef088c75b0398afc680800d3e315568b40a84
trombadoret <trombadoret@nih.gov>
2016-06-28 15:19:02 +0000

    Addition of spring, jax-rs, and cxf dependencies. Addition of new test rest web method. Existing error thrown on wildfly startup. Still confirming cxf vs. jersey jax-rs implementation.

---
+++ FormService/src/main/java/gov/nih/nci/cadsr/service/FormService.java
@@ -0,0 +1,18 @@
+package gov.nih.nci.cadsr.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class FormService {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/helloworld")
+	public Response helloWorld(){
+		return Response.ok("Hello World!").build();
+	}
+
+}
