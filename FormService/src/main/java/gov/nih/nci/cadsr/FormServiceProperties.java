dcc17ab4f7acfd6a29dde84e3942db4b2203ae61
trombadoret <trombadoret@nih.gov>
2016-08-12 20:14:44 +0000

    Updates to properties management and injection.

--- FormService/src/main/java/gov/nih/nci/cadsr/FormServiceProperties.java
+++ FormService/src/main/java/gov/nih/nci/cadsr/FormServiceProperties.java
@@ -3,20 +3,21 @@ package gov.nih.nci.cadsr;
 import org.springframework.beans.factory.annotation.Value;
 
 public class FormServiceProperties {
-	
+
 	private static final String DEFAULT_TEST_VALUE = "default test property";
-	
-	@Value("#{applicationProperties['test.value']}")
-	private String testProperty;
-	
-	public String getTestProp() {
 
-		if (testProperty == null || testProperty.isEmpty()) {
-			return DEFAULT_TEST_VALUE;
-		} else {
-			return testProperty;
+	@Value("${oracle.url}")
+	private String oracleUrl;
 
-		}
+	public String getOracleUrl() {
+		return oracleUrl;
+	}
+	
+	@Value("#{appProperties['testEnv']}")
+	private String testEnv;
+	
+	public String getTestEnv() {
+		return testEnv;
 	}
 
 }
