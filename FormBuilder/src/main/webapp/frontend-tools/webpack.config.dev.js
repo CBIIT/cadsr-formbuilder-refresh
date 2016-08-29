9ceda4f37f0cf3f3ef201d3cee050aade7419cd5
eldroubij <eldroubij@nih.gov>
2016-08-29 16:09:00 +0000

    CADSRFBTR-200  Add React to package.json, babel, webpack, ESLint

--- FormBuilder/src/main/webapp/frontend-tools/webpack.config.dev.js
+++ FormBuilder/src/main/webapp/frontend-tools/webpack.config.dev.js
@@ -1,14 +1,11 @@
 // For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
 import webpack from 'webpack';
-import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import autoprefixer from 'autoprefixer';
 
 /*
  import WebpackMd5Hash from 'webpack-md5-hash';
  */
-
-console.log("path resolve is:" + path.resolve('../webapp'));
 
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('development'),
@@ -19,6 +16,7 @@ export default {
   debug:   true,
   devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo:  true, // set to false to see a list of every file being bundled.
+  /*entry point to main JS and CSS file */
   entry:   ['./assets/js/main', './assets/styles/styles.scss'],
   target:  'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output:  {
@@ -30,6 +28,7 @@ export default {
     // Optimize the order that items are bundled. This assures the hash is deterministic.
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.ProvidePlugin({
+      /*make _ globally available */
       _: 'underscore'
     }),
     // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
@@ -49,7 +48,8 @@ export default {
         test:   /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',
-        query:  {presets: ['es2015']}
+        /* use these presets (specified in .babelrc */
+        query:  {presets: ['es2015', 'react']}
       },
       /*Underscore templates */
       {
@@ -70,5 +70,6 @@ export default {
         loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap!postcss')}
     ]
   },
+  /*used to add CSS vendor prefixes on SCSS transpilation to CSS rather than adding them manually in the SCSS source */
   postcss: ()=> [autoprefixer]
 };
