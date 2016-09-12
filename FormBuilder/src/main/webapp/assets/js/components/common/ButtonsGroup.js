e27c498299573356ad36cd0414811a5c7c2ea3f5
eldroubij <eldroubij@nih.gov>
2016-09-12 18:39:08 +0000

    CADSRFBTR-201: support an onClick callback for the buttonsGenerator

--- FormBuilder/src/main/webapp/assets/js/components/common/ButtonsGroup.js
+++ FormBuilder/src/main/webapp/assets/js/components/common/ButtonsGroup.js
@@ -2,8 +2,8 @@ import React, {Component, PropTypes} from 'react';
 import {Button} from 'react-bootstrap';
 
 const ButtonsGroup = (props) =>{
-	const createButton = ({type = "button", className = "btn btn-primary", name}, index) =>{
-		return <Button  type={type} className={className} key={index}>{name}</Button>;
+	const createButton = ({type = "button", className = "btn btn-primary", name, onClick =""}, index) =>{
+		return <Button onClick={onClick ? props[onClick] :''} type={type} className={className} key={index}>{name}</Button>;
 	};
 
 	return (
