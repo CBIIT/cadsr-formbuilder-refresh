88d96c2939d0bf22660927cfcbe7a9c7e80806ce
eldroubij <eldroubij@nih.gov>
2016-09-13 14:11:09 +0000

    CADSRFBTR-201: Add comment in code about module isEdited flag

--- FormBuilder/src/main/webapp/assets/js/models/forms/FormModuleModel.js
+++ FormBuilder/src/main/webapp/assets/js/models/forms/FormModuleModel.js
@@ -3,6 +3,7 @@ import {Model} from 'backbone';
 const FormModuleModel = Model.extend({
 	idAttribute: "moduleIdSeq",
 	defaults: {
+		/* isEdited is used for the backend to know if this is an existing or newly added module being sent to it */
 		isEdited: false,
 		longName: "",
 		instructions: ""
