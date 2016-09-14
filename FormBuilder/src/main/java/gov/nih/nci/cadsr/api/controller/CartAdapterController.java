2691f343d351b71de87a859a2fd3ecf0a0585999
betelhemebg <betelhemebg@nih.gov>
2016-09-14 22:19:09 +0000

    Translate CDE Cart XML data from Object Cart API into Java Objects

--- FormBuilder/src/main/java/gov/nih/nci/cadsr/api/controller/CartAdapterController.java
+++ FormBuilder/src/main/java/gov/nih/nci/cadsr/api/controller/CartAdapterController.java
@@ -1,11 +1,32 @@
 package gov.nih.nci.cadsr.api.controller;
 
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.oxm.XmlMappingException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +35,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.xml.sax.SAXException;
 
-import gov.nih.nci.cadsr.FormBuilderConstants;
 import gov.nih.nci.cadsr.FormBuilderProperties;
+import gov.nih.nci.cadsr.model.CartObjectNew;
+import gov.nih.nci.cadsr.model.Field;
+import gov.nih.nci.cadsr.model.HttpQuery;
+import gov.nih.nci.cadsr.model.Item;
+
 import gov.nih.nci.cadsr.model.BBDataElement;
 import gov.nih.nci.cadsr.model.BBModule;
+
 import gov.nih.nci.cadsr.model.session.SessionCarts;
+
+//import gov.nih.nci.cadsr.model.XMLConverter;
+
 import gov.nih.nci.ncicb.cadsr.common.CaDSRConstants;
 import gov.nih.nci.ncicb.cadsr.common.dto.FormTransferObject;
 import gov.nih.nci.ncicb.cadsr.common.dto.ModuleTransferObject;
 import gov.nih.nci.ncicb.cadsr.common.dto.QuestionTransferObject;
-import gov.nih.nci.ncicb.cadsr.common.resource.NCIUser;
-import gov.nih.nci.ncicb.cadsr.common.util.CDEBrowserParams;
 import gov.nih.nci.ncicb.cadsr.objectCart.CDECart;
 import gov.nih.nci.ncicb.cadsr.objectCart.CDECartItemTransferObject;
 import gov.nih.nci.ncicb.cadsr.objectCart.impl.CDECartOCImpl;
@@ -49,6 +77,9 @@ public class CartAdapterController {
 	@Autowired
 	private SessionCarts carts;
 
+	/*@Autowired
+	private XMLConverter xmlConverter;*/
+
 	@RequestMapping(value = "/moduleCart/{username}", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity getModuleCart(@RequestParam(value = "username", required = true) String username) {
@@ -59,7 +90,7 @@ public class CartAdapterController {
 
 	@RequestMapping(value = "/cdecart/{username}", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity getCDECart(@RequestParam(value = "username", required = true) String username) {
+	public ResponseEntity getCDECart(@PathVariable String username) {
 
 		return new ResponseEntity(carts.getCdeCart(), HttpStatus.OK);
 
@@ -73,7 +104,16 @@ public class CartAdapterController {
 
 	}
 
-	/*
+
+	/*@RequestMapping(value = "/objcart/cdecart/{username}", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity loadCDECart(@PathVariable String username) {
+
+
+		// CDEBrowserParams params = CDEBrowserParams.getInstance();
+		// String ocURL = "http://objcart2-dev.nci.nih.gov/objcart103";
+
+	
 	 * @RequestMapping(value = "/objcart/cdecart/{username}", method =
 	 * RequestMethod.GET)
 	 * 
@@ -102,11 +142,43 @@ public class CartAdapterController {
 	 * // return null;
 	 * 
 	 * }
-	 */
+	 
+
+		// CDEBrowserParams params = CDEBrowserParams.getInstance();
+		// String ocURL = params.getObjectCartUrl();
+		String ocURL = "http://objcart2-dev.nci.nih.gov/objcart103";
+
+		// Get the cart in the session
+		ObjectCartClient cartClient = null;
+
+		try {
+			
+			 * if (!ocURL.equals("")) cartClient = new ObjectCartClient(ocURL);
+			 * else
+			 
+			cartClient = new ObjectCartClient(ocURL);
+
+			CDECart userCart = new CDECartOCImpl(cartClient, username, CaDSRConstants.CDE_CART);
+
+			return new ResponseEntity(userCart.getDataElements(), HttpStatus.OK);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity(e.toString() + e.getMessage(), HttpStatus.OK);
+		}
+
+		// this.setSessionObject(request, CaDSRConstants.CDE_CART, userCart);
+
+		// return null;
+
+	}
+*/
+	
 
 	@RequestMapping(value = "/objcart/cdecart2/{username}", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity loadCDECart2(@PathVariable String username) {
+	public ResponseEntity loadCDECart2(@PathVariable String username) throws XmlMappingException, IOException,
+			SAXException, ParserConfigurationException, JAXBException, XMLStreamException {
 
 		String ocURL = "http://objcart2-dev.nci.nih.gov/objcart103";
 
@@ -122,12 +194,46 @@ public class CartAdapterController {
 		 * xml response. Converting from the xml "data" fields in the response
 		 * should produce a list of CDECartItemTransferObject
 		 */
-		CDECartItemTransferObject cde = new CDECartItemTransferObject();
 
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+		JAXBContext jaxbContext = JAXBContext.newInstance(HttpQuery.class);
+		URL url = new URL(uri);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		HttpQuery query = (HttpQuery) jaxbUnmarshaller.unmarshal(url);
 
-		return response;
+		List<CartObjectNew> cartContents = query.getQueryResponse().getCartContents();
+
+		List<Item> items = new ArrayList<Item>();
+		if (!cartContents.isEmpty()) {
+			for (CartObjectNew cartObj : cartContents) {
+				for (Field field : cartObj.getFields()) {
+					//JAXBElement<Item> item=null;
+
+					
+					
+					if (field.getName().equalsIgnoreCase("Data")) {
+						
+						XMLInputFactory xif = XMLInputFactory.newInstance();
+						XMLStreamReader xsr = xif.createXMLStreamReader(new StringReader(field.getValue()));
+						xsr.nextTag(); // Advance to CDECartItemTransferObject
+					    xsr.nextTag(); // Advance to item tag
+						JAXBContext jc = JAXBContext.newInstance(Item.class);
+						Unmarshaller unmarshaller = jc.createUnmarshaller();
+						JAXBElement<Item> je = unmarshaller.unmarshal(xsr, Item.class);
+						items.add(je.getValue());
+					}
+					
+				/*	if (field.getName().equalsIgnoreCase("dateAdded")) {
+						String data = field.getValue();
+						item.getValue().setDateAdded(data);
+					}*/
+					}
+			}
+
+		}
+
+		return new ResponseEntity<List<Item>>(items, HttpStatus.OK);
+
+
 	}
 
 	@RequestMapping(value = "/objcart/formcart/{username}", method = RequestMethod.GET)
