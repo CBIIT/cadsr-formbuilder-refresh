f4c8ccb165e43dfa9d90b98e7ad9b0cacbfd7e90
eldroubij <eldroubij@nih.gov>
2016-09-15 20:29:21 +0000

    CADSRFBTR-240: After successful fetch of a form object,  parse response and pass formMetadata and FormModules into their respective Backbone model/collection

--- FormBuilder/src/main/webapp/assets/js/models/forms/FormModel.js
+++ FormBuilder/src/main/webapp/assets/js/models/forms/FormModel.js
@@ -6,11 +6,19 @@ import FormMetadata from './FormMetadataModel';
 const FormModel = Model.extend({
 	/* Backbone tracks this module via the idAttribute, if it's set */
 	idAttribute: "formIdseq",
-	defaults: {
+	defaults:    {
 		formMetadata: new FormMetadata(),
 		formModules:  new FormModulesCollection()
 	},
-	urlRoot: ENDPOINT_URLS.FORM_SAVE
+	urlRoot:     ENDPOINT_URLS.FORMS,
+	parse(response){
+		const returnResponse = typeof response === "string" ? JSON.parse(response) : response;
+		/*marshalling nested objects/arrays into it's own collection to map to Backbone's nested model/collection  */
+		returnResponse.formMetadata = new FormMetadata(returnResponse.formModules);
+		returnResponse.formModules = new FormModulesCollection(returnResponse.formModules);
+		return returnResponse;
+
+	}
 });
 
 export default FormModel;
