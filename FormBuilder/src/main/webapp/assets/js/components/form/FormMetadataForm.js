25a4c5aea1f46257255daa6423f99b29bb1e0d62
eldroubij <eldroubij@nih.gov>
2016-09-15 20:59:35 +0000

    CADSRFBTR-241: FormMetaDataForm prepopulate text and textarea fields

--- FormBuilder/src/main/webapp/assets/js/components/form/FormMetadataForm.js
+++ FormBuilder/src/main/webapp/assets/js/components/form/FormMetadataForm.js
@@ -31,33 +31,35 @@ export default class FormMetadataForm extends Component {
 		mappedOptions.unshift({value: '', label: 'Select...'});
 		return mappedOptions;
 	}
+
 	render(){
 		return (
 			<Row>
 				<Form onSubmit={this.dispatchData} validatePristine={this.state.validatePristine} disabled={this.state.disabled} ref="formMetata">
 					<fieldset name={this.props.mainHeadingTitle}>
 						<legend>{this.props.mainHeadingTitle}</legend>
-						<Row> <Col sm={6}> <Input name="longName" id="longName" value="" label="
+						<Row> <Col sm={6}>
+							<Input value={this.props.formMetadata.longName} name="longName" id="longName" label="
 Long Name" type="text" required/>
-							<Textarea rows={3} cols={40} name="preferredDefinition" label="Preferred Definition" required/>
-							<Textarea rows={3} cols={40} name="headerInstructions" label="Header Instructions"/>
-							<Textarea rows={3} cols={40} name="footerInstructions" label="
+							<Textarea value={this.props.formMetadata.preferredDefinition} rows={3} cols={40} name="preferredDefinition" label="Preferred Definition" required/>
+							<Textarea value={this.props.formMetadata.headerInstructions} rows={3} cols={40} name="headerInstructions" label="Header Instructions"/>
+							<Textarea value={this.props.formMetadata.footerInstructions} rows={3} cols={40} name="footerInstructions" label="
 Footer Instructions"/> </Col> <Col sm={6}>
 
 							<FormGroup> <ControlLabel>Workflow</ControlLabel>
-								<FormControl.Static>{this.props.formMetadata.workflow}</FormControl.Static>
-							</FormGroup>
+								<FormControl.Static>{this.props.formMetadata.workflow}</FormControl.Static> </FormGroup>
 
 							<Select name="conteIdseq" label="Context" options={this.getOptions({
 								options:   this.props.uiDropDownOptionsModel.contexts,
 								optionKey: 'contextIdSeq'
 							})} required/> <Select name="formCategory" label="Category" options={this.getOptions({
 							options: this.props.uiDropDownOptionsModel.formCategories
-						})} /> <Select name="formType" label="Type" options={this.getOptions({
-							options: this.props.uiDropDownOptionsModel.formTypes})}  value={this.props.formMetadata.formType}
-						required/> <FormGroup> <ControlLabel>Version</ControlLabel>
-							<FormControl.Static>{this.props.formMetadata.version}</FormControl.Static>
-						</FormGroup> </Col> </Row>
+						})}/> <Select name="formType" label="Type" options={this.getOptions({
+							options: this.props.uiDropDownOptionsModel.formTypes
+						})} value={this.props.formMetadata.formType} required/> <FormGroup>
+							<ControlLabel>Version</ControlLabel>
+							<FormControl.Static>{this.props.formMetadata.version}</FormControl.Static> </FormGroup>
+						</Col> </Row>
 
 					</fieldset>
 					{this.props.children}
@@ -72,10 +74,10 @@ FormMetadataForm.defaultProps = {
 };
 
 FormMetadataForm.propTypes = {
-	mainHeadingTitle: PropTypes.string.isRequired,
-	children: PropTypes.node,
-	actionMode: PropTypes.string,
-	formMetadata: PropTypes.object.isRequired,
+	mainHeadingTitle:       PropTypes.string.isRequired,
+	children:               PropTypes.node,
+	actionMode:             PropTypes.string,
+	formMetadata:           PropTypes.object.isRequired,
 	uiDropDownOptionsModel: PropTypes.object.isRequired
 };
 
