7d625ad6eea504cbcb892a04898c9f5e1885fb58
trombadoret <trombadoret@nih.gov>
2016-09-16 20:18:48 +0000

    another logger issue fix

--- FormBuilder/src/main/java/gov/nih/nci/cadsr/api/controller/FormAdapterController.java
+++ FormBuilder/src/main/java/gov/nih/nci/cadsr/api/controller/FormAdapterController.java
@@ -1,7 +1,7 @@
 package gov.nih.nci.cadsr.api.controller;
 
 
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
