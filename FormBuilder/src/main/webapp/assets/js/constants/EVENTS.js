25c33f86eff6336926b96ad80d8abb568ca46640
eldroubij <eldroubij@nih.gov>
2016-09-20 15:55:40 +0000

    add comment to EVENTS.js about its purpose

--- FormBuilder/src/main/webapp/assets/js/constants/EVENTS.js
+++ FormBuilder/src/main/webapp/assets/js/constants/EVENTS.js
@@ -1,3 +1,7 @@
+/**
+ * event names meant to be used by Backbone Events/Backbone Radio
+ * @type {{APP: {SET_MAIN_CONTENT_LAYOUT: string}, FORM: {CREATE_FORM: string, EDIT_FORM: string, CREATE_MODULE: string, SET_NEW_MODULE: string, SAVE_FORM: string, SET_MODULE: string, SET_FORM_LAYOUT: string, GET_FORM_CORE_DETAILS_CRITERIA: string, SET_CORE_FORM_DETAILS: string, VIEW_MODULE: string}, SEARCH: {SEND_SEARCH_LAYOUT: string, SEND_SEARCH_INPUTS: string, RESULTS_COLLECTION_RESET: string}, USER: {GET_USERNAME: string}}}
+ */
 const EVENTS = {
 	APP:    {
 		SET_MAIN_CONTENT_LAYOUT: 'set:mainContentLayout'
