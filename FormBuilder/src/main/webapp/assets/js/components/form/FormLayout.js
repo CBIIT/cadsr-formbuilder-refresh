28b732a3158dd0dcaf96193b95a339907bb524a2
eldroubij <eldroubij@nih.gov>
2016-09-24 03:44:06 +0000

    CADSRFBTR-274: Disable Form Module form when navigating to it from Tree view and not being in Edit Mode

--- FormBuilder/src/main/webapp/assets/js/components/form/FormLayout.js
+++ FormBuilder/src/main/webapp/assets/js/components/form/FormLayout.js
@@ -163,13 +163,14 @@ export default class FormLayout extends Component {
 			const moduleEditing = this.getEditItems();
 			const buttons = [
 				{
-					name: "Save",
-					type: "submit"
+					name:     "Save",
+					type:     "submit",
+					disabled: !this.shouldShowFormEditControls()
 				}
 			];
 			/*Passing in moduleId here might not be necessary but currently the most straightforward way I can think of when there will be an array of modules (parent module, repetition) to edit and gather each one's id from the form when saving */
 			return (
-				<FormModuleForm moduleId={moduleEditing.id} longName={moduleEditing.longName} instructions={moduleEditing.instructions} actionMode={actionMode} mainHeadingTitle="Edit Module">
+				<FormModuleForm disabled={!this.shouldShowFormEditControls()} moduleId={moduleEditing.id} longName={moduleEditing.longName} instructions={moduleEditing.instructions} actionMode={actionMode} mainHeadingTitle="Edit Module">
 					<ButtonsGroup buttons={buttons}/> </FormModuleForm>
 			);
 		}
