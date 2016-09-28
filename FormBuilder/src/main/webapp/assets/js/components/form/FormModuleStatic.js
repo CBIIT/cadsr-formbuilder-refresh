14dc263d593621fc0cea11bf64f7a95a2a416f83
eldroubij <eldroubij@nih.gov>
2016-09-28 00:56:49 +0000

    CADSRFBTR-277: Add border around static module view

--- FormBuilder/src/main/webapp/assets/js/components/form/FormModuleStatic.js
+++ FormBuilder/src/main/webapp/assets/js/components/form/FormModuleStatic.js
@@ -3,16 +3,14 @@ import {Row, Col} from 'react-bootstrap';
 
 const formModuleStatic = (props) =>{
 	return (
-		<div>
-			<Row> <Col md={12}>
-				<h4>{props.longName}</h4>
-			</Col> </Row> <Row> <Col md={12}>
-			<h5>Instructions:</h5>					{props.instructions}
+		<Row className="bordered top-margin bottom-margin"> <Col md={12}> <Row> <Col md={12}>
+			<h4>{props.longName}</h4>
+		</Col> </Row> <Row> <Col md={12}>
+			<h5>Instructions:</h5>                    {props.instructions}
 		</Col> </Row> <Row> <Col lg={12}>
 			<h5>Questions:</h5>
 			<pre style={{height: "100px", whiteSpace: "normal"}}>{props.questions}</pre>
-		</Col> </Row>
-		</div>
+		</Col> </Row> </Col> </Row>
 	);
 };
 
