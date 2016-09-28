eba8296674b4f9da06711643489a22a6a3590f3f
eldroubij <eldroubij@nih.gov>
2016-09-28 13:42:41 +0000

    CADSRFBTR-277: In static module component, put Instructions label and content side by side

--- FormBuilder/src/main/webapp/assets/js/components/form/FormModuleStatic.js
+++ FormBuilder/src/main/webapp/assets/js/components/form/FormModuleStatic.js
@@ -5,8 +5,7 @@ const formModuleStatic = (props) =>{
 	return (
 		<Row className="bordered top-margin bottom-margin"> <Col md={12}> <Row> <Col md={12}>
 			<h4>{props.longName}</h4>
-		</Col> </Row> <Row> <Col md={12}>
-			<h5>Instructions:</h5>                    {props.instructions}
+		</Col> </Row> <Row> <Col md={12}> <span className="h5">Instructions:</span> {props.instructions}
 		</Col> </Row> <Row> <Col lg={12}>
 			<h5>Questions:</h5>
 			<pre style={{height: "100px", whiteSpace: "normal"}}>{props.questions}</pre>
