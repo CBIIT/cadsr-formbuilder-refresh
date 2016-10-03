c5dbc446604705483cdd6424364284b699d919e5
trombadorera <trombadorera@nih.gov>
2016-10-03 16:48:56 +0000

    updates to Question and ValidValue model objects

--- FormBuilder/src/main/java/gov/nih/nci/cadsr/model/BBQuestion.java
+++ FormBuilder/src/main/java/gov/nih/nci/cadsr/model/BBQuestion.java
@@ -8,13 +8,23 @@ public class BBQuestion {
 	private List<BBValidValue> validValues;
 	private int displayOrder;
 	private String deIdseq;
-	private BBDataElement bbdataElement;
+//	private BBDataElement bbdataElement;
 	private String instructions;
 	private String defaultValue;
-	private List<BBQuestionRepetition> questionRepetitions;
+//	private List<BBQuestionRepetition> questionRepetitions;
 	private boolean mandatory;
 	private boolean editable;
 	private boolean deDerived;
+	private Float version;
+	
+	private String preferredQuestionText;
+	private List<String> alternativeQuestionText;
+	private String longName;
+	private String dataType;
+	private String unitOfMeasure;
+	private String displayFormat;
+	private String concepts;
+	
 	
 	
 	public String getQuesIdseq() {
@@ -41,12 +51,12 @@ public class BBQuestion {
 	public void setDeIdseq(String deIdseq) {
 		this.deIdseq = deIdseq;
 	}
-	public BBDataElement getbbDataElement() {
-		return bbdataElement;
-	}
-	public void setbbDataElement(BBDataElement bbdataElement) {
-		this.bbdataElement = bbdataElement;
-	}
+//	public BBDataElement getbbDataElement() {
+//		return bbdataElement;
+//	}
+//	public void setbbDataElement(BBDataElement bbdataElement) {
+//		this.bbdataElement = bbdataElement;
+//	}
 	public String getInstructions() {
 		return instructions;
 	}
@@ -59,12 +69,12 @@ public class BBQuestion {
 	public void setDefaultValue(String defaultValue) {
 		this.defaultValue = defaultValue;
 	}
-	public List<BBQuestionRepetition> getQuestionRepetitions() {
-		return questionRepetitions;
-	}
-	public void setQuestionRepetitions(List<BBQuestionRepetition> questionRepetitions) {
-		this.questionRepetitions = questionRepetitions;
-	}
+//	public List<BBQuestionRepetition> getQuestionRepetitions() {
+//		return questionRepetitions;
+//	}
+//	public void setQuestionRepetitions(List<BBQuestionRepetition> questionRepetitions) {
+//		this.questionRepetitions = questionRepetitions;
+//	}
 	public boolean isMandatory() {
 		return mandatory;
 	}
@@ -82,6 +92,60 @@ public class BBQuestion {
 	}
 	public void setDeDerived(boolean deDerived) {
 		this.deDerived = deDerived;
+	}
+//	public BBDataElement getBbdataElement() {
+//		return bbdataElement;
+//	}
+//	public void setBbdataElement(BBDataElement bbdataElement) {
+//		this.bbdataElement = bbdataElement;
+//	}
+	public Float getVersion() {
+		return version;
+	}
+	public void setVersion(Float version) {
+		this.version = version;
+	}
+	public String getPreferredQuestionText() {
+		return preferredQuestionText;
+	}
+	public void setPreferredQuestionText(String preferredQuestionText) {
+		this.preferredQuestionText = preferredQuestionText;
+	}
+	public List<String> getAlternativeQuestionText() {
+		return alternativeQuestionText;
+	}
+	public void setAlternativeQuestionText(List<String> alternativeQuestionText) {
+		this.alternativeQuestionText = alternativeQuestionText;
+	}
+	public String getLongName() {
+		return longName;
+	}
+	public void setLongName(String longName) {
+		this.longName = longName;
+	}
+	public String getDataType() {
+		return dataType;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	public String getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+	public void setUnitOfMeasure(String unitOfMeasure) {
+		this.unitOfMeasure = unitOfMeasure;
+	}
+	public String getDisplayFormat() {
+		return displayFormat;
+	}
+	public void setDisplayFormat(String displayFormat) {
+		this.displayFormat = displayFormat;
+	}
+	public String getConcepts() {
+		return concepts;
+	}
+	public void setConcepts(String concepts) {
+		this.concepts = concepts;
 	}
 
 }
--- FormBuilder/src/main/java/gov/nih/nci/cadsr/model/BBValidValue.java
+++ FormBuilder/src/main/java/gov/nih/nci/cadsr/model/BBValidValue.java
@@ -1,101 +1,149 @@
 package gov.nih.nci.cadsr.model;
 
 public class BBValidValue {
-	
-	private String vdIdSeq;
-	private String vpIdSeq;
-	private String shortMeaning;
-	private String shortMeaningDescription;
-	private String shortMeaningValue;
-	private String description;
-	private Integer vmId;
+
+	// private String vdIdSeq;
+	private String vpIdseq;
+	private String valueIdseq;
+	// private String shortMeaning;
+	// private String shortMeaningDescription;
+	// private String shortMeaningValue;
+	// private String description;
+	// private Integer vmId;
 	private Float vmVersion;
-	private String beginDate;
-	private String endDate;
-	private String context;
-	private String workflowstatus;
-	private String instructions;
-	//ConceptDerivationRule?
-	//ValueMeaning?
-	
-	
-	public String getVdIdSeq() {
-		return vdIdSeq;
+	// private String beginDate;
+	// private String endDate;
+	// private String context;
+	// private String workflowstatus;
+	// private String instructions;
+	// ConceptDerivationRule?
+	// ValueMeaning?
+
+	private String longName;
+	private String formValueMeaningText;
+	private String formValueMeaningIdVersion;
+	private String formValueMeaningDesc;
+
+	// public String getVdIdSeq() {
+	// return vdIdSeq;
+	// }
+	// public void setVdIdSeq(String vdIdSeq) {
+	// this.vdIdSeq = vdIdSeq;
+	// }
+	public String getVpIdseq() {
+		return vpIdseq;
 	}
-	public void setVdIdSeq(String vdIdSeq) {
-		this.vdIdSeq = vdIdSeq;
+
+	public void setVpIdseq(String vpIdseq) {
+		this.vpIdseq = vpIdseq;
 	}
-	public String getVpIdSeq() {
-		return vpIdSeq;
+
+	public String getValueIdseq() {
+		return valueIdseq;
 	}
-	public void setVpIdSeq(String vpIdSeq) {
-		this.vpIdSeq = vpIdSeq;
+
+	public void setValueIdseq(String valueIdseq) {
+		this.valueIdseq = valueIdseq;
 	}
-	public String getShortMeaning() {
-		return shortMeaning;
-	}
-	public void setShortMeaning(String shortMeaning) {
-		this.shortMeaning = shortMeaning;
-	}
-	public String getShortMeaningDescription() {
-		return shortMeaningDescription;
-	}
-	public void setShortMeaningDescription(String shortMeaningDescription) {
-		this.shortMeaningDescription = shortMeaningDescription;
-	}
-	public String getShortMeaningValue() {
-		return shortMeaningValue;
-	}
-	public void setShortMeaningValue(String shortMeaningValue) {
-		this.shortMeaningValue = shortMeaningValue;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public Integer getVmId() {
-		return vmId;
-	}
-	public void setVmId(Integer vmId) {
-		this.vmId = vmId;
-	}
+
+	// public String getShortMeaning() {
+	// return shortMeaning;
+	// }
+	// public void setShortMeaning(String shortMeaning) {
+	// this.shortMeaning = shortMeaning;
+	// }
+	// public String getShortMeaningDescription() {
+	// return shortMeaningDescription;
+	// }
+	// public void setShortMeaningDescription(String shortMeaningDescription) {
+	// this.shortMeaningDescription = shortMeaningDescription;
+	// }
+	// public String getShortMeaningValue() {
+	// return shortMeaningValue;
+	// }
+	// public void setShortMeaningValue(String shortMeaningValue) {
+	// this.shortMeaningValue = shortMeaningValue;
+	// }
+	// public String getDescription() {
+	// return description;
+	// }
+	// public void setDescription(String description) {
+	// this.description = description;
+	// }
+	// public Integer getVmId() {
+	// return vmId;
+	// }
+	// public void setVmId(Integer vmId) {
+	// this.vmId = vmId;
+	// }
 	public Float getVmVersion() {
 		return vmVersion;
 	}
+
 	public void setVmVersion(Float vmVersion) {
 		this.vmVersion = vmVersion;
 	}
-	public String getBeginDate() {
-		return beginDate;
+
+	// public String getBeginDate() {
+	// return beginDate;
+	// }
+	// public void setBeginDate(String beginDate) {
+	// this.beginDate = beginDate;
+	// }
+	// public String getEndDate() {
+	// return endDate;
+	// }
+	// public void setEndDate(String endDate) {
+	// this.endDate = endDate;
+	// }
+	// public String getContext() {
+	// return context;
+	// }
+	// public void setContext(String context) {
+	// this.context = context;
+	// }
+	// public String getWorkflowstatus() {
+	// return workflowstatus;
+	// }
+	// public void setWorkflowstatus(String workflowstatus) {
+	// this.workflowstatus = workflowstatus;
+	// }
+	// public String getInstructions() {
+	// return instructions;
+	// }
+	// public void setInstructions(String instructions) {
+	// this.instructions = instructions;
+	// }
+	public String getLongName() {
+		return longName;
 	}
-	public void setBeginDate(String beginDate) {
-		this.beginDate = beginDate;
+
+	public void setLongName(String longName) {
+		this.longName = longName;
 	}
-	public String getEndDate() {
-		return endDate;
+
+	public String getFormValueMeaningText() {
+		return formValueMeaningText;
 	}
-	public void setEndDate(String endDate) {
-		this.endDate = endDate;
+
+	public void setFormValueMeaningText(String formValueMeaningText) {
+		this.formValueMeaningText = formValueMeaningText;
 	}
-	public String getContext() {
-		return context;
+
+	public String getFormValueMeaningIdVersion() {
+		return formValueMeaningIdVersion;
 	}
-	public void setContext(String context) {
-		this.context = context;
+
+	public void setFormValueMeaningIdVersion(String formValueMeaningIdVersion) {
+		this.formValueMeaningIdVersion = formValueMeaningIdVersion;
 	}
-	public String getWorkflowstatus() {
-		return workflowstatus;
+
+	public String getFormValueMeaningDesc() {
+		return formValueMeaningDesc;
 	}
-	public void setWorkflowstatus(String workflowstatus) {
-		this.workflowstatus = workflowstatus;
-	}
-	public String getInstructions() {
-		return instructions;
-	}
-	public void setInstructions(String instructions) {
-		this.instructions = instructions;
+
+	public void setFormValueMeaningDesc(String formValueMeaningDesc) {
+		this.formValueMeaningDesc = formValueMeaningDesc;
 	}
 
 }
