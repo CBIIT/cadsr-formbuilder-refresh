1569129a96e25c00eeddc918c6ca19db68d6fa22
eldroubij <eldroubij@nih.gov>
2016-10-12 02:37:02 +0000

    CADSRFBTR-337: Enable saving of Valid value. Fix backboneModelHelpers.getDeepModelPojo not add cid of nested collections and models.

--- FormBuilder/src/main/webapp/assets/js/helpers/backboneModelHelpers.js
+++ FormBuilder/src/main/webapp/assets/js/helpers/backboneModelHelpers.js
@@ -5,18 +5,29 @@ const backboneModelHelpers = {
 	 */
 	getDeepModelPojo(model){
 		let json = _.clone(model.attributes);
-		function checkKeys(json) {
-			for(let attr in json) {
-				if(json[attr] && (json[attr].attributes) || (json[attr].models)) {
-					json[attr] = json[attr].toJSON();
+
+		function checkKeys(json){
+			for(let attr in json){
+				if(json[attr] && (json[attr].attributes) || (json[attr].models)){
+					if(json[attr].attributes){
+						json[attr] = Object.assign({}, json[attr].attributes, json[attr].cid);
+					}
+					else if(json[attr].models){
+						json[attr] = json[attr].models.map((model) =>{
+							return Object.assign({}, model.attributes, {cid: model.cid});
+						});
+					}
 					checkKeys(json[attr]);
 				}
 				/*TODO Fix hardcoding of validValues here*/
-				else if (json[attr].validValues) {
-					json[attr].validValues = json[attr].validValues.toJSON();
+				else if(json[attr].validValues){
+					json[attr].validValues = json[attr].validValues.models.map((model) =>{
+						return Object.assign({}, model.attributes,  {cid: model.cid});
+					});
 				}
 			}
 		}
+
 		checkKeys(json);
 		return json;
 
