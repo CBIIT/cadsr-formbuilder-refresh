8b96874ab4dfe523b9bf08391b0c66d78fd3bd59
eldroubij <eldroubij@nih.gov>
2016-10-13 02:37:35 +0000

    CADSRFBTR-337: Comment out console log

--- FormBuilder/src/main/webapp/assets/js/components/form/form-modules/QuestionEditable.js
+++ FormBuilder/src/main/webapp/assets/js/components/form/form-modules/QuestionEditable.js
@@ -56,10 +56,12 @@ export default class QuestionEditable extends Component {
 	handleQuestionChanged(currentValues, isChanged) {
 		/* BE AWARE: Form.onChange returns true unexpectedly. Using isChanged as guard */
 		if(isChanged) {
+/*
+			console.log("a QuestionEditable form changed");
+*/
 			/*TODO Move this somewhere else
 			 * sets the string "true"/"false" to boolean  */
 			currentValues.mandatory = JSON.parse(currentValues.mandatory.toLowerCase());
-			console.log("a QuestionEditable form changed");
 
 			formChannel.trigger(EVENTS.FORM.SET_QUESTION,
 				{moduleId: this.props.moduleId,
