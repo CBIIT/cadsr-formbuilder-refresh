2a0a10f620aac503c78044bde1b45e5feb8a67af
eldroubij <eldroubij@nih.gov>
2016-10-17 18:10:23 +0000

    CADSRFBTR-322: Fix getFormCartCollectionPojo not handing null protocol and context for a form in cart

--- FormBuilder/src/main/webapp/assets/js/helpers/CartDataHelpers.js
+++ FormBuilder/src/main/webapp/assets/js/helpers/CartDataHelpers.js
@@ -1,27 +1,30 @@
-
-export const getCdeCartCollectionPojo = (cdeCollection) => {
+export const getCdeCartCollectionPojo = (cdeCollection) =>{
 	return cdeCollection.models.map(model =>{
 		return Object.assign({}, model.attributes.dataElement, {id: model.cid});
 	});
 };
 
-export const getFormCartCollectionPojo = (formCartCollection) => {
+export const getFormCartCollectionPojo = (formCartCollection) =>{
 	return formCartCollection.models.map(model =>{
-		return Object.assign({}, model.attributes, {id: model.cid, contextName: model.get('context').name, protocolLongName: model.get('protocols')[0].longName});
+		return Object.assign({}, model.attributes, {
+			id:               model.cid,
+			contextName:      (model.get('context')) ? model.get('context').name : '',
+			protocolLongName: (model.get('protocols')) ? model.get('protocols')[0].longName : ''
+		});
 	});
 };
 
-export const getModuleCartCollectionPojo = (formCartCollection) => {
+export const getModuleCartCollectionPojo = (formCartCollection) =>{
 	return formCartCollection.models.map(model =>{
 		return {
-			longName: model.attributes.longName,
-			instructions: model.attributes.instructions,
-			id: model.attributes.moduleIdseq,
-			numQuestions:  model.attributes.questions.length,
+			longName:           model.attributes.longName,
+			instructions:       model.attributes.instructions,
+			id:                 model.attributes.moduleIdseq,
+			numQuestions:       model.attributes.questions.length,
 			originFormLongName: model.attributes.form.longName,
-			originFormContext: model.attributes.form.context,
+			originFormContext:  model.attributes.form.context,
 			originFormPublicId: model.attributes.form.publicId,
-			originFormVersion: model.attributes.form.version
+			originFormVersion:  model.attributes.form.version
 		};
 	});
 };
