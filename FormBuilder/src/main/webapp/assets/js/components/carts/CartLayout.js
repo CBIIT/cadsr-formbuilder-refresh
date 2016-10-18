6cd34c26d1556d1c364622f3d75d17d3e02ed5f3
eldroubij <eldroubij@nih.gov>
2016-10-18 14:37:39 +0000

    CADSRFBTR-317, CADSRFBTR-322, 	CADSRFBTR-321: change pagination back to 100 per page

--- FormBuilder/src/main/webapp/assets/js/components/carts/CartLayout.js
+++ FormBuilder/src/main/webapp/assets/js/components/carts/CartLayout.js
@@ -76,7 +76,7 @@ export default class CartLayout extends Component {
 			return (
 				<div>
 					<h1 className="text--bold">Form Builder | {pageName} Cart</h1>
-					<Datatable cartLastSortedState={cartLastSortedState} pagination={true} perPage={10} pageName={pageName} columnTitles={columnConfig} data={this.data}></Datatable>
+					<Datatable cartLastSortedState={cartLastSortedState} pagination={true} perPage={100} pageName={pageName} columnTitles={columnConfig} data={this.data}></Datatable>
 				</div>
 			);
 		}
