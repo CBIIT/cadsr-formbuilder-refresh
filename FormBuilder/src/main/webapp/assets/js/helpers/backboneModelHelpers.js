6d0b68f751c845162d1a497349e42003e8246ba3
eldroubij <eldroubij@nih.gov>
2016-10-26 01:39:11 +0000

    CADSRFBTR-208: Change BackboneModelHelpers DeepCopyPojo to include models' cid optionally

--- FormBuilder/src/main/webapp/assets/js/helpers/backboneModelHelpers.js
+++ FormBuilder/src/main/webapp/assets/js/helpers/backboneModelHelpers.js
@@ -1,9 +1,10 @@
 const backboneModelHelpers = {
 	/**
-	 * Recursively run toJSON on a backbone model or backbone collection
+	 * Recursively run toJSON on a backbone model or backbone collection and optionally include the model's cid
 	 * @param model
+	 * @param includeCid
 	 */
-	getDeepModelPojo(model){
+	getDeepModelPojo(model, includeCid = true){
 		let json = _.clone(model.attributes);
 
 		function checkKeys(json){
@@ -11,11 +12,11 @@ const backboneModelHelpers = {
 
 				if(json[attr] && (json[attr].attributes || json[attr].models)){
 					if(json[attr].attributes){
-						json[attr] = Object.assign({}, json[attr].attributes, json[attr].cid);
+						json[attr] = Object.assign({},  json[attr].attributes, (includeCid ? {cid: json[attr].cid} : {}));
 					}
 					else if(json[attr].models){
 						json[attr] = json[attr].models.map((model) =>{
-							return Object.assign({}, model.attributes, {cid: model.cid});
+							return Object.assign({}, model.attributes, (includeCid ? {cid: model.cid} : {}));
 						});
 					}
 					checkKeys(json[attr]);
@@ -23,8 +24,7 @@ const backboneModelHelpers = {
 				/*TODO Fix hardcoding of validValues here*/
 				else if(json[attr] && json[attr].validValues){
 					json[attr].validValues = json[attr].validValues.models.map((model) =>{
-						return Object.assign({}, model.attributes,  {cid: model.cid});
-					});
+						return Object.assign({}, model.attributes, (includeCid ? {cid: model.cid} : {}));					});
 				}
 			}
 		}
