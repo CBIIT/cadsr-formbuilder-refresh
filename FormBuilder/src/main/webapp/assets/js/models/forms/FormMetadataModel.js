2677855296382dfaba9d4310a64f724a9bdf776e
eldroubij <eldroubij@nih.gov>
2016-11-01 20:06:34 +0000

    Remove default protoIdseq being added via FormMeatadataModel defaults

--- FormBuilder/src/main/webapp/assets/js/models/forms/FormMetadataModel.js
+++ FormBuilder/src/main/webapp/assets/js/models/forms/FormMetadataModel.js
@@ -6,7 +6,7 @@ const FormMetadataModel = Model.extend({
 		createdBy:           '',
 		longName:            '',
 		protocols:           [{
-			protoIdseq: "1B80E8BB-7546-7F0B-E050-BB89AD437420",
+			protoIdseq: "",
 			longName: ""
 		}],
 		classifications:	 [{
