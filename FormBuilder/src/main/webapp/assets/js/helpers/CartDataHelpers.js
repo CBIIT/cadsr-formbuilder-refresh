f05be307ae4729185a155707b58f8dc19f9ac2b6
eldroubij <eldroubij@nih.gov>
2016-11-03 22:17:25 +0000

    Fix Form Cart Table not handling an empty array of protocols, and to display more than one protocol

--- FormBuilder/src/main/webapp/assets/js/helpers/CartDataHelpers.js
+++ FormBuilder/src/main/webapp/assets/js/helpers/CartDataHelpers.js
@@ -2,16 +2,23 @@ export const getCdeCartCollectionPojo = (cdeCollection) =>{
 	return cdeCollection.models.map(model =>{
 		return Object.assign({}, model.attributes.dataElement, {
 			parentQuestionModelId: model.id,
-			id: model.attributes.deIdseq});
+			id:                    model.attributes.deIdseq
+		});
 	});
 };
 
 export const getFormCartCollectionPojo = (formCartCollection) =>{
 	return formCartCollection.models.map(model =>{
+		const mapArrayItems = (items, key) =>{
+			return items.map((item) =>{
+				return `${item[key]} `;
+			});
+		};
+
 		return Object.assign({}, model.attributes, {
-			id:    model.attributes.formIdseq,
+			id:               model.attributes.formIdseq,
 			contextName:      (model.get('context')) ? model.get('context').name : '',
-			protocolLongName: (model.get('protocols')) ? model.get('protocols')[0].longName : ''
+			protocolLongName: mapArrayItems(model.get('protocols'), "longName")
 		});
 	});
 };
@@ -24,10 +31,10 @@ export const getModuleCartCollectionPojo = (moduleCartCollection) =>{
 			instructions:       model.attributes.instructions,
 			id:                 model.attributes.moduleIdseq || model.cid,
 			numQuestions:       model.attributes.numQuestions,
-			originFormLongName: (moduleFormObject) ? moduleFormObject.longName: '',
-			originFormContext:  (moduleFormObject) ? moduleFormObject.context.name: '',
-			originFormPublicId: (moduleFormObject) ? moduleFormObject.publicId: '',
-			originFormVersion:  (moduleFormObject) ? moduleFormObject.version: ''
+			originFormLongName: (moduleFormObject) ? moduleFormObject.longName : '',
+			originFormContext:  (moduleFormObject) ? moduleFormObject.context.name : '',
+			originFormPublicId: (moduleFormObject) ? moduleFormObject.publicId : '',
+			originFormVersion:  (moduleFormObject) ? moduleFormObject.version : ''
 		};
 	});
 };
